86460e1c3a9df08cf3debdafd4f0f015ad3bb54f
Paul Holser <pholser@alumni.rice.edu>
2012-02-08 11:45:46 -0600

    prime factors theories with data points and quick-check supplied values

--- src/test/java/com/pholser/junit/theories/PrimeFactorsQuickCheckTheories.java
+++ src/test/java/com/pholser/junit/theories/PrimeFactorsQuickCheckTheories.java
@@ -1,10 +1,9 @@
 package com.pholser.junit.theories;
 
 import com.pholser.junit.parameterized.PrimeFactors;
-import org.junit.contrib.theories.DataPoints;
+import com.pholser.junit.quickcheck.ForAll;
 import org.junit.contrib.theories.Theories;
 import org.junit.contrib.theories.Theory;
-import org.junit.experimental.theories.DataPoint;
 import org.junit.runner.RunWith;
 
 import java.math.BigInteger;
@@ -14,22 +13,9 @@ import static org.junit.Assert.*;
 import static org.junit.Assume.*;
 
 @RunWith(Theories.class)
-public class PrimeFactorsTheories {
-    @DataPoint public static final int ONE = 1;
-    @DataPoint public static final int TWO = 2;
-    @DataPoint public static final int THREE = 3;
-    @DataPoints public static int[] moreExamples = { 4, 5, 6 };
-
-    @DataPoint public static int anotherExample() {
-        return 5;
-    }
-
-    @DataPoints public static int[] stillMoreExamples() {
-        return new int[] { 6, 7, 8, 9 };
-    }
-
+public class PrimeFactorsQuickCheckTheories {
     @Theory
-    public void factorsPassPrimalityTest(int n) {
+    public void factorsPassPrimalityTest(@ForAll int n) {
         assumeThat(n, greaterThan(0));
 
         for (int each : PrimeFactors.of(n))
@@ -37,7 +23,7 @@ public class PrimeFactorsTheories {
     }
 
     @Theory
-    public void factorsMultiplyToOriginal(int n) {
+    public void factorsMultiplyToOriginal(@ForAll int n) {
         assumeThat(n, greaterThan(0));
 
         int product = 1;
@@ -48,7 +34,7 @@ public class PrimeFactorsTheories {
     }
 
     @Theory
-    public void factorizationsAreUnique(int m, int n) {
+    public void factorizationsAreUnique(@ForAll int m, @ForAll int n) {
         assumeThat(m, greaterThan(0));
         assumeThat(n, greaterThan(0));
         assumeThat(m, not(equalTo(n)));
